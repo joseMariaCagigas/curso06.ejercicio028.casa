414b260ce3a40ecc1ef39ae0bda61cf77236d605
curso06 <curso06java@gmail.com>
2017-03-02 13:31:03 +0100

    Salvando primeros datos

---
+++ src/main/java/es/cic/curso/curso06/ejercicio028/backend/dominio/Genero.java
@@ -0,0 +1,57 @@
+package es.cic.curso.curso06.ejercicio028.backend.dominio;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import es.cic.curso.curso06.ejercicio028.backend.repository.Identificable;
+
+@Entity
+@Table(name = "GENERO")
+public class Genero implements Identificable<Long> {
+	private static final long serialVersionUID = -8800715225024553533L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+
+	@Column(name = "nombre")
+	private String nombre;
+
+	@Column(name = "descripcion")
+	private String descripcion;
+
+	@OneToMany(mappedBy = "programa")
+	private List<Genero> generos = new ArrayList<>();
+
+	/**
+	 * @return the id
+	 */
+	@Override
+	public Long getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	@Override
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+
+
+
+}
