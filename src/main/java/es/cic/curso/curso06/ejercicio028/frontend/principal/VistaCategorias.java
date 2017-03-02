6462cf47ebbf99e5e3e114e2a75d2cd05df31ce0
cursojava <cursojava@BSVM-CURSOJAVA>
2017-03-02 21:24:27 +0100

    Introducción de elementos de las otras vistas

--- src/main/java/es/cic/curso/curso06/ejercicio028/frontend/principal/VistaCategorias.java
+++ src/main/java/es/cic/curso/curso06/ejercicio028/frontend/principal/VistaCategorias.java
@@ -1,7 +1,21 @@
 package es.cic.curso.curso06.ejercicio028.frontend.principal;
 
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Grid;
 import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.TextField;
 
 public class VistaCategorias extends HorizontalLayout {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -6039462805912435329L;
+
+	private TextField buscador;
+	private Button crear, borrar, actualizar, aceptar, cancelar;
+	private Grid gridCategorias;
+	private Label label;
+	
 }
--- src/main/java/es/cic/curso/curso06/ejercicio028/frontend/principal/VistaGeneros.java
+++ src/main/java/es/cic/curso/curso06/ejercicio028/frontend/principal/VistaGeneros.java
@@ -1,7 +1,21 @@
 package es.cic.curso.curso06.ejercicio028.frontend.principal;
 
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Grid;
 import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.TextField;
 
 public class VistaGeneros extends HorizontalLayout {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 5366004381410718812L;
+
+	private TextField buscador;
+	private Button crear, borrar, actualizar, aceptar, cancelar;
+	private Grid gridCategorias;
+	private Label label;
+	
 }
