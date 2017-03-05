c8bf2c096f5bb384513dc22c3805aeed9572f630
cursojava <cursojava@BSVM-CURSOJAVA>
2017-03-05 13:24:41 +0100

    Agregando test a las clases nuevas

---
+++ src/test/java/es/cic/curso/curso06/ejercicio028/backend/repository/CanalRepositoryImplTest.java
@@ -0,0 +1,44 @@
+package es.cic.curso.curso06.ejercicio028.backend.repository;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class CanalRepositoryImplTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testAdd() {
+		assertTrue(true);
+	}
+
+	@Test
+	public void testRead() {
+		assertTrue(true);
+	}
+
+	@Test
+	public void testList() {
+		assertTrue(true);
+	}
+
+	@Test
+	public void testDeleteK() {
+		assertTrue(true);
+	}
+
+	@Test
+	public void testDeleteT() {
+		assertTrue(true);
+	}
+
+	@Test
+	public void testUpdate() {
+		assertTrue(true);
+	}
+
+}
