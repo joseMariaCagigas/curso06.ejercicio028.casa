ddaf8e3274a44bd992ba5bcbd12125f24e398527
cursojava <cursojava@BSVM-CURSOJAVA>
2017-03-05 10:57:21 +0100

    Categoria completada

--- src/test/java/es/cic/curso/curso06/ejercicio028/backend/repository/CategoriaRepositoryImplTest.java
+++ src/test/java/es/cic/curso/curso06/ejercicio028/backend/repository/CategoriaRepositoryImplTest.java
@@ -1,54 +1,81 @@
 package es.cic.curso.curso06.ejercicio028.backend.repository;
 
-import static org.junit.Assert.*;
-
 import org.junit.Before;
-import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
-import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
-import org.springframework.transaction.annotation.Transactional;
+
+import es.cic.curso.curso06.ejercicio028.backend.dominio.Categoria;
+
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "classpath:es/cic/curso/curso06/ejercicio028/applicationContext.xml" })
-@TestExecutionListeners({ DependencyInjectionTestExecutionListener.class, TransactionalTestExecutionListener.class })
-@Transactional
-public class CategoriaRepositoryImplTest {
 
-	@Before
-	public void setUp() throws Exception {
+public class CategoriaRepositoryImplTest extends AbstractRepositoryImplTest<Long, Categoria>{
+
+
+	 @Autowired
+	    private CategoriaRepository sut;
+
+	    @Before
+	    @Override
+	    public void setUp() throws Exception {
+	        super.setUp();
+	    }
+
+	    @Override
+	    public Categoria getInstanceDeTParaNuevo() {
+	    	
+	    	Categoria op = new Categoria();
+	        
+	        op.setDescripcion("categoria");
+	      
+	        
+	        return op;
+	    }
+
+	    @Override
+	    public Categoria getInstanceDeTParaLectura() {
+	    	
+	    	Categoria op = new Categoria();
+	        
+	        op.setDescripcion("categoria");
+	      
+
+	        return op;
+	    }
+
+	    @Override
+	    public Long getClavePrimariaNoExistente() {
+	        return Long.MAX_VALUE;
+	    }
+
+	    @Override
+	    public Categoria getInstanceDeTParaModificar(Long clave) {
+	    	Categoria op = getInstanceDeTParaLectura();
+	        op.setId(clave);
+	        op.setDescripcion("operacion");
+	       
+	        return op;
+	    }
+
+	    @Override
+	    public IRepository<Long, Categoria> getRepository() {
+	        return sut;
+	    }
+
+	    @Override
+	    public boolean sonDatosIguales(Categoria t1, Categoria t2) {
+	        if (t1 == null || t2 == null) {
+	            throw new UnsupportedOperationException("No puedo comparar nulos");
+	        }
+	        
+			if (!t1.getDescripcion().equals(t2.getDescripcion())) {
+				return false;
+			}
+			
+		 
+	        
+	        return true;
+	    }
 	}
-
-	@Test
-	public void testAdd() {
-		assertTrue(true);
-	}
-
-	@Test
-	public void testRead() {
-		assertTrue(true);
-	}
-
-	@Test
-	public void testList() {
-		assertTrue(true);
-	}
-
-	@Test
-	public void testDeleteK() {
-		assertTrue(true);
-	}
-
-	@Test
-	public void testDeleteT() {
-		assertTrue(true);
-	}
-
-	@Test
-	public void testUpdate() {
-		assertTrue(true);
-	}
-
-}
