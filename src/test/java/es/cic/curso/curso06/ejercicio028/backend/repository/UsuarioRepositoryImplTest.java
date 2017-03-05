6eb5d417032a67031cc30065b289e88c71eb8bca
cursojava <cursojava@BSVM-CURSOJAVA>
2017-03-05 10:59:21 +0100

    Acabando test repositorio para usuario

--- src/test/java/es/cic/curso/curso06/ejercicio028/backend/repository/UsuarioRepositoryImplTest.java
+++ src/test/java/es/cic/curso/curso06/ejercicio028/backend/repository/UsuarioRepositoryImplTest.java
@@ -6,16 +6,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import es.cic.curso.curso06.ejercicio028.backend.dominio.Genero;
+import es.cic.curso.curso06.ejercicio028.backend.dominio.Usuario;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "classpath:es/cic/curso/curso06/ejercicio028/applicationContext.xml" })
 
-public class GeneroRepositoryImplTest extends AbstractRepositoryImplTest<Long, Genero>{
+public class UsuarioRepositoryImplTest extends AbstractRepositoryImplTest<Long, Usuario>{
 
 
 	 @Autowired
-	    private GeneroRepository sut;
+	    private UsuarioRepository sut;
 
 	    @Before
 	    @Override
@@ -24,22 +24,22 @@ public class GeneroRepositoryImplTest extends AbstractRepositoryImplTest<Long, G
 	    }
 
 	    @Override
-	    public Genero getInstanceDeTParaNuevo() {
+	    public Usuario getInstanceDeTParaNuevo() {
 	    	
-	    	Genero op = new Genero();
+	    	Usuario op = new Usuario();
 	        
-	        op.setDescripcion("categoria");
+	        op.setApellidos("categoria");
 	      
 	        
 	        return op;
 	    }
 
 	    @Override
-	    public Genero getInstanceDeTParaLectura() {
+	    public Usuario getInstanceDeTParaLectura() {
 	    	
-	    	Genero op = new Genero();
+	    	Usuario op = new Usuario();
 	        
-	        op.setDescripcion("categoria");
+	        op.setApellidos("categoria");
 	      
 
 	        return op;
@@ -51,26 +51,26 @@ public class GeneroRepositoryImplTest extends AbstractRepositoryImplTest<Long, G
 	    }
 
 	    @Override
-	    public Genero getInstanceDeTParaModificar(Long clave) {
-	    	Genero op = getInstanceDeTParaLectura();
+	    public Usuario getInstanceDeTParaModificar(Long clave) {
+	    	Usuario op = getInstanceDeTParaLectura();
 	        op.setId(clave);
-	        op.setDescripcion("operacion");
+	        op.setApellidos("operacion");
 	       
 	        return op;
 	    }
 
 	    @Override
-	    public IRepository<Long, Genero> getRepository() {
+	    public IRepository<Long, Usuario> getRepository() {
 	        return sut;
 	    }
 
 	    @Override
-	    public boolean sonDatosIguales(Genero t1, Genero t2) {
+	    public boolean sonDatosIguales(Usuario t1, Usuario t2) {
 	        if (t1 == null || t2 == null) {
 	            throw new UnsupportedOperationException("No puedo comparar nulos");
 	        }
 	        
-			if (!t1.getDescripcion().equals(t2.getDescripcion())) {
+			if (!t1.getApellidos().equals(t2.getApellidos())) {
 				return false;
 			}
 			
